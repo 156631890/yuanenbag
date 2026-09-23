9bf1fb1cf44576ee6263e72e1e902a8caba29bfa
xbai8596 <18657789961@163.com>
2026-09-23 15:31:33 +0800

    Add red bakery cooler gallery images

--- src/photographed-products.ts
+++ src/photographed-products.ts
@@ -72,7 +72,7 @@ const createCustomCooler = (slug:string, enColor:string, zhColor:string, esColor
 
 export const additionalInsulatedBags:Bag[] = [
   createCustomCooler('coffee-insulated-cooler-bags','Coffee','咖啡色','café','coffee-insulated-cooler-bag'),
-  createCustomCooler('red-insulated-cooler-bags','Red','红色','rojo','red-insulated-cooler-bag'),
+  createCustomCooler('red-insulated-cooler-bags','Red','红色','rojo','red-insulated-cooler-bag',['red-insulated-cooler-bag-meal','red-insulated-cooler-bag-bakery','red-insulated-cooler-bag-fresh-food','red-insulated-cooler-bag-drinks']),
   createCustomCooler('pink-insulated-cooler-bags','Pink','粉色','rosa','pink-insulated-cooler-bag',['pink-insulated-cooler-bag-meal','pink-insulated-cooler-bag-office','pink-insulated-cooler-bag-fresh-food','pink-insulated-cooler-bag-sandwich']),
   createCustomCooler('yellow-insulated-cooler-bags','Yellow','黄色','amarillo','yellow-insulated-cooler-bag',['yellow-insulated-cooler-bag-meal','yellow-insulated-cooler-bag-grocery','yellow-insulated-cooler-bag-coffee','yellow-insulated-cooler-bag-bakery','yellow-insulated-cooler-bag-open']),
 ]
